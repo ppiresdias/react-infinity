8ca0a5af07d91de547d78e50e4a0f8e72ef866bb
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-09-15 09:36:24 -0400

    using contextapi as a global state

--- assets/js/components/Advance/Advance.js
+++ assets/js/components/Advance/Advance.js
@@ -1,19 +1,48 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
+const AppContext = React.createContext();
+
+class Provider extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Patty',
+			clicked: false
+		};
+	}
+
+	clickedButton = () => {
+		this.setState({
+			clicked: !this.state.clicked
+		});
+	};
+
+	render() {
+		return (
+			<AppContext.Provider
+				value={{ globalState: this.state, clickedButton: this.clickedButton }}
+			>
+				{this.props.children}
+			</AppContext.Provider>
+		);
+	}
+}
 export default class Advanced extends Component {
 	constructor() {
 		super();
 		this.state = {
-			name: 'Patty'
+			name: 'Patty&Bryan'
 		};
 	}
 
 	render() {
 		return (
-			<div id="GrandComp">
-				<GrandComp name={this.state.name} />
-			</div>
+			<Provider>
+				<div id="advancedFeatures">
+					<GrandComp />
+				</div>
+			</Provider>
 		);
 	}
 }
@@ -21,7 +50,7 @@ export default class Advanced extends Component {
 const GrandComp = props => {
 	return (
 		<div className="GrandComp">
-			<FatherComp name={props.name} />
+			<FatherComp />
 		</div>
 	);
 };
@@ -29,11 +58,35 @@ const GrandComp = props => {
 const FatherComp = props => {
 	return (
 		<div className="FatherComp">
-			<ChildComp name={props.name} />
+			<ChildComp />
 		</div>
 	);
 };
 
 const ChildComp = props => {
-	return <div className="ChildComp">{props.name}</div>;
+	return (
+		// <div className="ChildComp">
+		// 	<AppContext.Consumer>
+		// 		{context => <>{context.globalState.name}</>}
+		// 	</AppContext.Consumer>
+		// </div>
+
+		// or
+		<AppContext.Consumer>
+			{context => (
+				<>
+					<div
+						className={`ChildComp ${
+							context.globalState.clicked === true ? 'active' : ''
+						}`}
+					>
+						{context.globalState.name}
+						<div className="button" onClick={context.clickedButton}>
+							Save
+						</div>
+					</div>
+				</>
+			)}
+		</AppContext.Consumer>
+	);
 };
