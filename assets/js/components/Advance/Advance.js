d56808c7f87ba61ba065bcdf646b9547b13a132a
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-09-15 08:14:29 -0400

    Passing down props - beginner

--- assets/js/components/Advance/Advance.js
+++ assets/js/components/Advance/Advance.js
@@ -1,44 +1,39 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-console.dir(document.getElementById('testing'));
-
 export default class Advanced extends Component {
 	constructor() {
 		super();
-		this.state = {};
-		this.bryanRef = React.createRef();
-	}
-
-	componentDidMount() {
-		// console.dir(document.getElementById('bryan'));
-		console.dir(this.bryanRef);
+		this.state = {
+			name: 'Patty'
+		};
 	}
 
 	render() {
 		return (
-			<div id="Advanced">
-				<div ref={this.bryanRef}>Bryan</div>
-				<ChildComp total={6} />
+			<div id="GrandComp">
+				<GrandComp name={this.state.name} />
 			</div>
 		);
-		//Fragment: directly return what's imbedded within parent component without having to add a parent element
-		// use fragment tag to pass in props, keys, properties, etc.
-		// return <Fragment>Text</Fragment>;
 	}
 }
 
+const GrandComp = props => {
+	return (
+		<div className="GrandComp">
+			<FatherComp name={props.name} />
+		</div>
+	);
+};
+
+const FatherComp = props => {
+	return (
+		<div className="FatherComp">
+			<ChildComp name={props.name} />
+		</div>
+	);
+};
+
 const ChildComp = props => {
-	return <div>{props.total}</div>;
-};
-
-// pass in default props in case no props are passed down
-ChildComp.defaultProps = {
-	total: 3
-};
-
-// ensure total is a number, error if its another data type
-// useful for debugging
-ChildComp.propTypes = {
-	total: PropTypes.number
+	return <div className="ChildComp">{props.name}</div>;
 };
