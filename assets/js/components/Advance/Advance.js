fca0af498732a3d8b235dca8305e0c64dc76cc70
Patricia Pires Dias <ppiresdias@umassd.edu>
2020-09-14 22:20:30 -0400

    using refs to find a dom node
    using refs to find a dom node without adding a class or id to the element

--- assets/js/components/Advance/Advance.js
+++ assets/js/components/Advance/Advance.js
@@ -1,15 +1,27 @@
 import React, { Component, Fragment } from 'react';
 
+console.dir(document.getElementById('testing'));
+
 export default class Advanced extends Component {
 	constructor() {
 		super();
 		this.state = {};
+		this.bryanRef = React.createRef();
+	}
+
+	componentDidMount() {
+		// console.dir(document.getElementById('bryan'));
+		console.dir(this.bryanRef);
 	}
 
 	render() {
-		// return <div id="Advanced">Text</div>;
+		return (
+			<div id="Advanced">
+				<div ref={this.bryanRef}>Bryan</div>
+			</div>
+		);
 		//Fragment: directly return what's imbedded within parent component without having to add a parent element
 		// use fragment tag to pass in props, keys, properties, etc.
-		return <Fragment>Text</Fragment>;
+		// return <Fragment>Text</Fragment>;
 	}
 }
